362051095c5613193e08d62e8c3303eb934d2728
guilhermecsr <gcsramos@id.uff.br>
2022-08-12 18:20:50 -0300

    implementing navbar and some refactors

--- spec/requests/csv_controller_spec.rb
+++ spec/requests/csv_controller_spec.rb
@@ -3,9 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe CsvController, type: :request do
-  let(:politician) { FactoryBot.create(:politico) }
-  let!(:expense) { FactoryBot.create(:despesa, politico: politician) }
-
   describe 'POST/create' do
     let!(:params) { { csv: fixture_file_upload('spec/fixtures/Ano-2020-test.csv', 'file/csv') } }
     before { post csv_index_path, params: params }
