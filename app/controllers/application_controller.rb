d50dd234c5cff0be9fb56c25b949864caaf86a38
guilhermecsr <gcsramos@id.uff.br>
2022-08-12 01:12:11 -0300

    rescue errors and refactoring some views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found(exception)
+    flash[:error] = "Erro ao encontrar #{exception.model}. Você foi redirecionado!"
+    redirect_to action: :index
+  end
 end
